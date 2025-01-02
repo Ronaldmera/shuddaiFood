336d61f3e71cd3979501517ac1d9531fbe634114
Ronaldmera <ronaldalme1230@gmail.com>
2025-01-02 15:32:57 -0500

    update map

--- script/map.js
+++ script/map.js
@@ -1,21 +1,32 @@
-// Coordenadas reales (reemplaza con las tuyas)
+// Coordenadas reales 
 const lat = 2.678809; // Latitud
 const lng = -76.562124; // Longitud
 
 // Inicializar el mapa con las coordenadas reales
-const map = L.map('map').setView([lat, lng], 13); // Cambia el nivel de zoom si es necesario
+const map = L.map('map', {
+  scrollWheelZoom: false, // Desactivar el zoom con la rueda del ratón
+  touchZoom: false,       // Desactivar el zoom táctil
+  dragging: false         // Desactivar el arrastre por defecto
+}).setView([lat, lng], 13); // Cambia el nivel de zoom si es necesario
+
+// Activar el movimiento y el zoom solo cuando el usuario haga clic
+map.on('click', function() {
+  map.dragging.enable();       // Habilitar el arrastre
+  map.scrollWheelZoom.enable(); // Habilitar el zoom con la rueda del ratón
+  map.touchZoom.enable();      // Habilitar el zoom táctil
+});
 
 // Agregar una capa de mapa (OpenStreetMap)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 // Añadir un marcador en las coordenadas reales
 L.marker([lat, lng])
   .addTo(map)
   .bindPopup(`
-    <div style="font-size: 16px; font-weight: bold; color: #E56A48;; text-align: center;">
+    <div style="font-size: 16px; font-weight: bold; color: #E56A48; text-align: center;">
       ¡Aquí estamos! <br> El Shaddai Food.
     </div>
   `)
@@ -23,8 +34,8 @@ L.marker([lat, lng])
 
 // Añadir un círculo rojo alrededor de las coordenadas
 var circle = L.circle([lat, lng], {
-    color: '#E56A48',          // Borde del círculo
-    fillColor: '#E56A48',     // Color de relleno
-    fillOpacity: 0.3,      // Opacidad del relleno
-    radius: 900            // Radio en metros
+  color: '#E56A48',          // Borde del círculo
+  fillColor: '#E56A48',     // Color de relleno
+  fillOpacity: 0.3,         
+  radius: 900                
 }).addTo(map);
