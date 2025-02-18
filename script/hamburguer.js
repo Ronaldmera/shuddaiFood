136028664f8aeab157a6e458001771ccbb68733f
Ronaldmera <ronaldalme1230@gmail.com>
2025-02-18 12:54:19 -0500

    aggregated hamburger menu

---
+++ script/hamburguer.js
@@ -0,0 +1,13 @@
+const hamburguer = document.querySelector('.hamburguer');
+const enlaces = document.querySelector('#nav-links');
+
+hamburguer.addEventListener('click', () => {
+    enlaces.classList.toggle('show');
+
+});
+// Cerrar el menú si se hace clic fuera de él
+document.addEventListener("click", function (event) {
+    if (!enlaces.contains(event.target) && !hamburguer.contains(event.target)) {
+        enlaces.classList.remove("show");
+    }
+});
