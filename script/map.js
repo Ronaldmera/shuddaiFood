153a59cda4d15876d821e00b05cbfa05f1f609fc
Ronaldmera <ronaldalme1230@gmail.com>
2026-01-07 20:27:36 -0500

    animaciones scrollReveal agregadas

--- script/map.js
+++ script/map.js
@@ -1,41 +1,44 @@
-// Coordenadas reales 
-const lat = 9.187460; // Latitud
+// Coordenadas reales
+const lat = 9.18746; // Latitud
 const lng = -75.555972; // Longitud
 
 // Inicializar el mapa con las coordenadas reales
-const map = L.map('map', {
+const map = L.map("map", {
   scrollWheelZoom: false, // Desactivar el zoom con la rueda del ratón
-  touchZoom: false,       // Desactivar el zoom táctil
-  dragging: false         // Desactivar el arrastre por defecto
+  touchZoom: false, // Desactivar el zoom táctil
+  dragging: false, // Desactivar el arrastre por defecto
 }).setView([lat, lng], 13); // Cambia el nivel de zoom si es necesario
 
 // Activar el movimiento y el zoom solo cuando el usuario haga clic
-map.on('click', function() {
-  map.dragging.enable();       // Habilitar el arrastre
+map.on("click", function () {
+  map.dragging.enable(); // Habilitar el arrastre
   map.scrollWheelZoom.enable(); // Habilitar el zoom con la rueda del ratón
-  map.touchZoom.enable();      // Habilitar el zoom táctil
+  map.touchZoom.enable(); // Habilitar el zoom táctil
 });
 
 // Agregar una capa de mapa (OpenStreetMap)
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
 // Añadir un marcador en las coordenadas reales
 L.marker([lat, lng])
   .addTo(map)
-  .bindPopup(`
+  .bindPopup(
+    `
     <div style="font-size: 16px; font-weight: bold; color: #E56A48; text-align: center;">
       ¡Aquí estamos! <br> El Shuddai Food.
     </div>
-  `)
+  `
+  )
   .openPopup();
 
 // Añadir un círculo rojo alrededor de las coordenadas
 var circle = L.circle([lat, lng], {
-  color: '#E56A48',          // Borde del círculo
-  fillColor: '#E56A48',     // Color de relleno
-  fillOpacity: 0.3,         
-  radius: 900                
+  color: "#E56A48", // Borde del círculo
+  fillColor: "#E56A48", // Color de relleno
+  fillOpacity: 0.3,
+  radius: 900,
 }).addTo(map);
--- script/txtWelcome.js
+++ script/txtWelcome.js
@@ -2,19 +2,37 @@ let texto = "Comida rápida que sí vale la pena";
 let index = 0;
 
 let mostrarTexto = () => {
-    if (index < texto.length) {
-        document.querySelector('#welcome-title').textContent += texto[index];
-        index++;
-        setTimeout(mostrarTexto, 80);
-    }
+  if (index < texto.length) {
+    document.querySelector("#welcome-title").textContent += texto[index];
+    index++;
+    setTimeout(mostrarTexto, 80);
+  }
 };
 
 mostrarTexto();
 
-
 //  manejo del icono del boton del Navbar en movil
-let btnNav = document.querySelector(".navbar-toggler")
-let ico = document.querySelector(".ico")
-btnNav.addEventListener('click',()=>{
-    ico.classList.toggle("navbar-toggler-icon-close");
-})
+let btnNav = document.querySelector(".navbar-toggler");
+let ico = document.querySelector(".ico");
+btnNav.addEventListener("click", () => {
+  ico.classList.toggle("navbar-toggler-icon-close");
+});
+
+// animacione scrollReveal
+ScrollReveal({ reset: false }).reveal(".text-animation", {
+  delay: 300,
+  distance: "40px",
+  duration: 800,
+  easing: "ease-out",
+  origin: "left",
+});
+ScrollReveal({ reset: false }).reveal(".text-animation2", {
+  delay: 300,
+  distance: "40px",
+  duration: 800,
+  easing: "ease-out",
+  origin: "right",
+});
+ScrollReveal({ reset: false }).reveal(".text-animation-fade", {
+  delay: 300,
+});
